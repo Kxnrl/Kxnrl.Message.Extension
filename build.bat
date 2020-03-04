44583f874afec1c9ce266c126011a212d6847bae
PerfectLaugh <1749462-PerfectLaugh@users.noreply.gitlab.com>
2020-03-05 04:45:55 +0800

    Call vcvarsall in build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
 set EXT_DIR=%cd%
+set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+
+if "%VSCMD_VER%"=="" (
+	set MAKE=
+	set CC=
+	set CXX=
+	call %VCVARSALL% x86 8.1
+)
 
 git clone https://github.com/alliedmodders/sourcemod --recursive --branch %BRANCH% --single-branch %EXT_DIR%/sourcemod-%BRANCH%
 
