2958e18d67bb5b333ba2063e677aca8082815000
PerfectLaugh <1749462-PerfectLaugh@users.noreply.gitlab.com>
2020-03-05 06:15:30 +0800

    Enable RTTI

--- vcpkg_triplet/x86-linux-sm.cmake
+++ vcpkg_triplet/x86-linux-sm.cmake
@@ -1,8 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CXX_FLAGS "-fno-rtti")
-set(VCPKG_C_FLAGS "")
 set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
