b55aac6c7ea02e51076c5782dbf09a1ffdc000f3
PerfectLaugh <1749462-PerfectLaugh@users.noreply.gitlab.com>
2020-03-04 16:54:47 +0800

    Fix build scripts

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-EXT_DIR=%cd%
+set EXT_DIR=%cd%
 
 git clone https://github.com/alliedmodders/sourcemod --recursive --branch %BRANCH% --single-branch %EXT_DIR%/sourcemod-%BRANCH%
 
 mkdir build
 pushd build
-python ../configure.py --enable-optimize --sm-path %EXT_DIR% && ambuild
+python ../configure.py --enable-optimize --sm-path %EXT_DIR%/sourcemod-%BRANCH% && ambuild
 popd
--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 EXT_DIR=$(pwd)
 
-git clone https://github.com/alliedmodders/sourcemod --recursive --branch $BRANCH --single-branch $(pwd)/sourcemod-$BRANCH
+git clone https://github.com/alliedmodders/sourcemod --recursive --branch $BRANCH --single-branch $EXT_DIR/sourcemod-$BRANCH
 
 mkdir build
 pushd build
-python ../configure.py --enable-optimize --sm-path $EXT_DIR && ambuild
+python ../configure.py --enable-optimize --sm-path $EXT_DIR/sourcemod-$BRANCH && ambuild
 popd
